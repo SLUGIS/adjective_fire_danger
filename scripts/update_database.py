ff3a4f4dc6ec42aec7aa91ac8c672ae21faa1721
hunter-waite <hwaite@calpoly.edu>
2020-09-14 09:08:05 -0700

    Switched from firestore to realtime database

--- scripts/update_database.py
+++ scripts/update_database.py
@@ -2,6 +2,7 @@ import update_sheet
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import db
 from pprint import pprint
 
 
@@ -11,38 +12,40 @@ def main():
     the adjective fire danger for the unit app"""
     # get credentials for the firestore and initialize app
     cred = credentials.Certificate('unitapp_admin.json')
-    firebase_admin.initialize_app(cred)
-    # obtain a database reference
-    db = firestore.client()
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://unitapp-61a16.firebaseio.com/'
+    })
+
+    # obtain a database reference to smokey the bear
+    ref = db.reference('Smokey')
 
     date = update_sheet.get_formatted_date()
 
     # update the document for the Coastal smokey bear
-    update_document(db, "SLO", "Coastal", date)
+    update_document(ref, "SLO", "Coastal", date)
     # update the document for the Inland smokey bear
-    update_document(db, "LAS_TABLAS", "Inland", date)
+    update_document(ref, "LAS_TABLAS", "Inland", date)
 
 
-def update_document(db, station, region, correct_date): 
+def update_document(ref, station, region, correct_date):
     """ Updates the documnets that hold data for the smokey bear
 
     Args:
-        db: Firestore Database reference
+        ref: Real Time Database reference
         station: Station name associated with the text file holding its data
         region: The region that the station is in (Coastal or Inland)
         correct_date: The correct date to compare against the update
 
     """
+
     date, rating = update_sheet.get_date_and_rating(station)
-    smokey_ref = db.collection('Smokey').document(region)
 
     if date != correct_date:
         date = "Wrong Date Data"
 
-    smokey_ref.set({
-        'Date'      : date,
-        'Rating'    : rating
-    })   
+    ref.update({
+        region: rating
+    })
 
 
 if __name__ == "__main__":
